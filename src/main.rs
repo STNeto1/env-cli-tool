7129c0dbb6249647cfca690a8daf98ef44407c9f
STNeto1 <gfreitasneto18@gmail.com>
2023-03-14 17:13:29 -0300

    init config file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
+use std::fs::{self, File};
+
+use anyhow::{bail, Result};
 use clap::Parser;
 use cli::{Action, Args};
 
 mod cli;
 
-fn main() {
+const PATH: &str = "config.toml";
+
+fn main() -> Result<()> {
     let args = Args::parse();
 
     match args.action {
-        Action::Init => println!("init"),
+        Action::Init => init()?,
         Action::Add(props) => println!("add -> k:{} v:{}", props.key, props.value),
         Action::List => println!("list"),
         Action::Remove(props) => println!("remove -> k:{}", props.key),
     };
+
+    return Ok(());
+}
+
+fn init() -> Result<()> {
+    if fs::read_to_string(PATH).is_ok() {
+        bail!("file already exists");
+    }
+
+    let mut _output = File::create(PATH)?;
+    return Ok(());
 }
